5f1046da10ec03e10cd37a330248278f291c27d7
rs028 <rob.sommariva@gmail.com>
2023-06-07 10:19:05 +0100

    Scripts to run on UoB BlueBEAR

---
+++ BlueBEAR/submit_parallel.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --ntasks 3
+#SBATCH --nodes 1
+#SBATCH --time 3:30:00
+#SBATCH --account=levinejg-gi4raq
+#SBATCH --qos=bbdefault
+#SBATCH --mail-type ALL
+
+set -e
+
+module purge; module load bluebear
+module load Python/3.8.6-GCCcore-10.2.0 
+module load geopandas/0.9.0-foss-2020b
+module load tqdm/4.56.2-GCCcore-10.2.0
+module load numba/0.52.0-foss-2020b
+
+python settings_parallel.py
+
+
