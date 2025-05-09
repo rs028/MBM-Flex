63d32c1dcf08b113b9f47d6a22a7a693f05b6bc5
Neil Butcher <n.a.butcher@bham.ac.uk>
2025-05-09 11:00:08 +0100

    working inchempy test

--- multiroom_model_tests/test_inchem_py.py
+++ multiroom_model_tests/test_inchem_py.py
@@ -4,7 +4,9 @@ from inchempy import modules
 
 class TestInChemPy(unittest.TestCase):
     def test_inchempy_runs(self):
-        inchem_py_instance: InChemPyInstance = InChemPyInstance()
+        inchem_py_instance: InChemPyInstance = InChemPyInstance(
+            seconds_to_integrate=180,
+            output_graph=False)
 
         inchem_py_instance.run()
 
